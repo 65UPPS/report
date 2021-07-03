4bf537d2659cd97e51218d8fce48d2d156a6f8bc
65UPPS <38510885+65UPPS@users.noreply.github.com>
2021-07-04 07:17:43 +1100

    Delete authorization.py

--- authorization.py
+++
@@ -1,4 +0,0 @@
-DB_HOST = 'ec2-54-74-14-109.eu-west-1.compute.amazonaws.com'
-DB_NAME = "de0fglpocdb2up"
-DB_USER = "jgpjdpqktnjurl"
-DB_PASS = "479cbe7cb2935b6151a1145e1bce96e7575bcecfd5d581ea214210358d1b0172"
